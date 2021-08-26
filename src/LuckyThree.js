1ecca7f49d9339a47febdf70816f45684a3298ba
Nickfff-dev <hunchographics@gmail.com>
2021-08-26 19:20:40 +0300

    tuma kitu

--- src/LuckyThree.js
+++ src/LuckyThree.js
@@ -42,7 +42,7 @@ class LuckyThree extends React.Component {
     }
     
   tryLuckClickHandler (e) {
-        this.checkAllCards.bind(e)
+       return this.checkAllCards.bind(e)
     }
     
     render() {
@@ -62,7 +62,7 @@ class LuckyThree extends React.Component {
                 {
                     /* TODO 5: Add the onClick handler to the input tag and bind it with the created function in TODO 4 called "tryLuckClickHandler" */
                 }
-                <input id="tryYourLuckBtn" className="custom-btn" type="button" value="Try Your Luck" onClick={this.tryLuckClickHandler.bind(this)} />
+                <input id="tryYourLuckBtn" className="custom-btn" type="button" value="Try Your Luck" onClick={this.tryLuckClickHandler.bind(this.id)} />
             </div>
         )
     }
